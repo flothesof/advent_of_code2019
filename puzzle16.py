06e23d2c304c70d9a37c8bbf32bd6a552d39d06c
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2019-12-16 20:30:00 +0100

    finished puzzle16

--- puzzle16.py
+++ puzzle16.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def gen_pattern(output_elem):
     pattern = []
     base_pattern = [0, 1, 0, -1]
@@ -23,7 +26,7 @@ def pattern_iter(pattern, index=0, skipped_first=False):
 
 
 def take(n, iter):
-    """Returns first n values from the given sequence."""
+    """Returns first n values from the given iterator."""
     result = []
     try:
         for i in range(n):
@@ -73,59 +76,27 @@ assert apply_ntimes(list(map(int, "80871224585914546619083218645595")), 100)[:8]
 assert apply_ntimes(list(map(int, "19617804207202209144916044189917")), 100)[:8] == list(map(int, "73745418"))
 assert apply_ntimes(list(map(int, "69317163492948606335995924319873")), 100)[:8] == list(map(int, "52432133"))
 
+input_signal = list(map(int, open('data/input16').read().strip()))
+print(f"solution for part1: {''.join(map(str, apply_ntimes(input_signal, 100)[:8]))}")
 
-# print(f"solution for part1: {''.join(map(str, apply_ntimes(list(map(int, open('data/input16').read().strip())), 100)[:8]))}")
 
-
-def apply2(input_signal, debug=False):
-    output_signal = []
-    for output_phase in range(1, len(input_signal) + 1):
-        if debug: debug_str = ""
-        iter = pattern_iter(gen_pattern(output_phase), index=output_phase, skipped_first=True)
-        s = 0
-        for i, p in zip(input_signal[output_phase - 1:], iter):
-            s += i * p
-            if debug: debug_str += f"{i: 2}*{p: 2} + "
-        s = keep_ones_digit(s)
-        if debug: debug_str = debug_str[:-3] + f" = {s}"; print(debug_str)
-        output_signal.append(s)
+# part2
+def apply_second_ntimes_second_half_only(input_signal, n=1):
+    N = len(input_signal)
+    second = input_signal[N // 2:]
+    output_signal = second[:]
+    for _ in range(n):
+        output_signal = abs(np.cumsum(output_signal[::-1])[::-1]) % 10
     return output_signal
 
 
-take(15, pattern_iter(gen_pattern(2)))
-take(15, pattern_iter(gen_pattern(2), index=1, skipped_first=True))
+assert apply_ntimes(list(map(int, "12345678")), 2)[-4:] == apply_second_ntimes_second_half_only(
+    list(map(int, "12345678")), 2).tolist()
+assert apply_ntimes(list(map(int, "12345678")), 20)[-4:] == apply_second_ntimes_second_half_only(
+    list(map(int, "12345678")), 20).tolist()
 
-apply(list(map(int, "12345678")), debug=True)
-apply2(list(map(int, "12345678")), debug=True)
-assert apply2(list(map(int, "12345678"))) == list(map(int, "48226158"))
-
-
-def apply_ntimes2(input_signal, n):
-    signal = input_signal[:]
-    for _ in range(n):
-        signal = apply2(signal)
-    return signal
-
-
-assert apply_ntimes2(list(map(int, "12345678")), 4) == list(map(int, "01029498"))
-assert apply_ntimes2(list(map(int, "80871224585914546619083218645595")), 100)[:8] == list(map(int, "24176176"))
-assert apply_ntimes2(list(map(int, "19617804207202209144916044189917")), 100)[:8] == list(map(int, "73745418"))
-assert apply_ntimes2(list(map(int, "69317163492948606335995924319873")), 100)[:8] == list(map(int, "52432133"))
-
-# part2 investigate symetry:
-apply(list(map(int, "12345678")), debug=True)
-apply2(list(map(int, "12345678")), debug=True)
-import numpy as np
-import matplotlib.pyplot as plt
-
-input_len = 10
-coef_mat = np.empty((input_len, input_len))
-for output_phase in range(1, input_len + 1):
-    iter = pattern_iter(gen_pattern(output_phase))
-    row = take(input_len, iter)
-    print(row)
-    coef_mat[output_phase - 1] = row
-
-plt.imshow(coef_mat)
-
-#print(f"solution for part1: {''.join(map(str, apply_ntimes2(list(map(int, open('data/input16').read().strip())), 100)[:8]))}")
+long_input_signal = input_signal * 10000
+offset = int("".join(map(str, long_input_signal[:7])))
+start = offset - len(long_input_signal) // 2
+second_half = apply_second_ntimes_second_half_only(long_input_signal, n=100)
+print(f"solution for part2: {''.join(map(str, second_half[start: start + 8]))}")
